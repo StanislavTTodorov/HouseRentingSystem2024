6f773377d9a45e7fc7adeee9cbf0249c08a0f85c
Stanislav Todorov <77863355+StanislavTTodorov@users.noreply.github.com>
2024-02-28 12:34:23 +0200

    Add

--- HouseRentingSystem2024/Program.cs
+++ HouseRentingSystem2024/Program.cs
@@ -1,38 +1,41 @@
 
-
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-
-
-builder.Services.AddApplicationDbContext(builder.Configuration);
-builder.Services.AddApplicationIdentiry(builder.Configuration);
-
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddApplicationServises();
-
-WebApplication app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseMigrationsEndPoint();
-    app.UseDeveloperExceptionPage();
+    private static async Task Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddApplicationDbContext(builder.Configuration);
+        builder.Services.AddApplicationIdentiry(builder.Configuration);
+
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddApplicationServises();
+
+        WebApplication app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapDefaultControllerRoute();
+        app.MapRazorPages();
+
+        await app.RunAsync();
+    }
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");   
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapDefaultControllerRoute();
-app.MapRazorPages();
-
-app.Run();
